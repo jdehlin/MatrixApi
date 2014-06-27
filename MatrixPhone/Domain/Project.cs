131bdb132b11086cd126e06631e76687f28fd703
Casey Neehouse <casey@bestcasetech.com>
2014-06-27 13:04:19 -0400

    Windows Phone 8.1 Project Added.

---
+++ MatrixPhone/Domain/Project.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using NHibernate.Mapping;
+
+namespace MatrixApi.Domain
+{
+    public class Project
+    {
+        private IList<Ticket> _tickets = new List<Ticket>();
+
+        public virtual int Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Description { get; set; }
+        public virtual Priority Priority { get; set; }
+        public virtual Status Status { get; set; }
+        public virtual DateTime? Deadline { get; set; }
+
+        public virtual IList<Ticket> Tickets
+        {
+            get { return _tickets; }
+            set { _tickets = value; }
+        }
+    }
+}
